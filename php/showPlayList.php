d1ab6634389bf84bf3100fa086b06bcf4546676b
fifichin <fifichin926@gmail.com>
2020-01-03 22:09:27 +0800

    add login.php

--- php/showPlayList.php
+++ php/showPlayList.php
@@ -1,14 +1,19 @@
 <?php
 try {
-  require_once("./connectBooks.php");
-  $sql = "select * 
-          from allplaylist ap join singleplaylist sp on(ap.plist_no = sp.plist_no)
-          where ap.mem_no = 1;";
-  $allList = $pdo->query($sql);
-  $listRow = $allList->fetch(PDO::FETCH_ASSOC);
-
-  json_encode($listRow);
+    require_once("./connectBooks.php");
+    $sql = "select * from `member`;";
+    $allList = $pdo->query($sql);
+    // $listRow = $allList->fetch(PDO::FETCH_ASSOC);
+   
+    if ($allList->rowCount() == 0) {
+        echo "Account or password is ERROR, please<a href='login.html'>Log in again</a>";
+    } else {   
+         $memRow = $allList->fetch(PDO::FETCH_ASSOC);
+        echo "Hello<br>";
+    };
+    //   echo json_encode($memRow);
 } catch (PDOException $e) {
-  echo "例外行號:", $e->getLine(), "<br>";
-  echo "錯誤訊息:", $e->getMessage(), "<br>";
-};
+    echo "例外行號:", $e->getLine(), "<br>";
+    echo "錯誤訊息:", $e->getMessage(), "<br>";
+}
+?>
