0009075a6760c1de1192e8c61032d1e3115d80c6
fifichin <fifichin926@gmail.com>
2020-01-08 20:59:27 +0800

    add 0108

--- php/connectBooks.php
+++ php/connectBooks.php
@@ -1,6 +1,6 @@
 <?php
 $dsn = "mysql:host=localhost; port=3306; dbname=dd104g1; charset=utf8";
 $user = "root";
-$password = "tibame";
+$password = "liltwind529";
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
