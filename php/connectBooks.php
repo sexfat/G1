8100c2e7ee0e4ceb80f12250ecb11c01b4158497
ozzero <eeac8133@gmail.com>
2020-01-12 18:44:26 +0800

    0112add

--- php/connectBooks.php
+++ php/connectBooks.php
@@ -1,7 +1,7 @@
 <?php
 $dsn = "mysql:host=localhost;port=3306;dbname=dd104g1;charset=utf8";
-$user = "root";
-$password = "456123789";
+$user = "onezero";
+$password = "acta228give331";
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
 ?>
--- php/songupload.php
+++ php/songupload.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
+<?php 
+  try {
+	require_once("connectBooks.php");
+	$sql = "insert into dd104g1(song_pic,song_name,song_addr,cat_no,song_idn)";
+    $songsadd = $pdo -> prepare($sql);
+    $songsadd->bindValue('song_pic',$_POST['song_pic']);
+    $songsadd->bindValue('song_name',$_POST['song_name']);
+    $songsadd->execute();
+    if( $songsadd->rowCount() != 0){
+        $addRow = $songsadd->fetch(PDO::FETCH_ASSOC);
+        $songpic=$_POST["song_pic"];
+        $songpic=$_POST["song_name"];
+        echo "Successly";
+    }else{
+        echo "Failure";
+    }
+  } catch (PDOException $e) {
+	echo "例外行號 : ", $e->getLine(),"<br>";
+	echo "例外原因 : ", $e->getMessage(),"<br>";		
+  }
+  ?> 
 </body>
 </html>
