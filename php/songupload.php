c99049cd0346d3e562031669d5f3cac75e558e98
ozzero <eeac8133@gmail.com>
2020-01-13 13:25:41 +0800

    add

--- php/songupload.php
+++ php/songupload.php
@@ -1,15 +1,18 @@
 <?php 
   try {
 	require_once("connectBooks.php");
-	$sql = "insert into dd104g1(song_pic,song_name,song_addr,cat_no,song_idn)";
+    $sql = "Insert into `total_station_music_library`(`song_no`,`song_name`,`song_date`)
+            values(`song_no`=:song_no,`song_name`=:song_name,`song_date`=:song_date)";
     $songsadd = $pdo -> prepare($sql);
-    $songsadd->bindValue('song_pic',$_POST['song_pic']);
-    $songsadd->bindValue('song_name',$_POST['song_name']);
+    // $songsadd->bindValue('song_pic',$_POST['song_pic']);
+    $songsadd->bindValue('song_name',$_GET['song_name']);
+    $songsadd->bindValue('song_no',11);
+    $songsadd->bindValue('song_date',2020-01-12);
     $songsadd->execute();
     if( $songsadd->rowCount() != 0){
         $addRow = $songsadd->fetch(PDO::FETCH_ASSOC);
-        $songpic=$_POST["song_pic"];
-        $songpic=$_POST["song_name"];
+        // $songpic=$_POST["song_pic"];
+        $songpic=$_GET["song_name"];
         echo "Successly";
     }else{
         echo "Failure";
