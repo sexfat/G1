f06b65801611e482a5da632b8df3d285d451099c
Alessandra8566 <money80202@yahoo.com.tw>
2020-01-15 12:44:53 +0800

    1/15

---
+++ js/memberSession.js
@@ -0,0 +1,13 @@
+//get member login  information
+function getLoginInfo(){
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+      member=JSON.parse(xhr.responseText);
+    //   if(member.memName){
+    //   }
+    }  
+    xhr.open("post", url, true);
+    xhr.setRequestHeader("content-type","application/x-www-form-urlencoded");
+    let member_data = "mem_acct=" + document.getElementById('mem_acct').value+"&&mem_psw=" + document.getElementById('mem_psw').value ;
+    xhr.send(member_data);   
+}
--- phps/store.php
+++ phps/store.php
@@ -1,11 +1,35 @@
 <?php
-  try {
-    require_once('./connect_dd104g1.php');
-    $sql = "INSERT INTO `stored value record`(`store_no`, `mem_no`, `store_price`, `store_day`) VALUES (null,{$_GET['mem_no']},{$_GET['store_price']},'{$_GET['store_day']}')";
-    $add = $pdo -> prepare($sql);
-    $add -> execute();
-    echo "Stored value successfully";
-  } catch (PDOException $th) {
-    echo $th -> getMessage(),"<br>";
-  }
+session_start();
+
+switch ($_GET['get']) {
+  case "storemoney":
+    try {
+      require_once('./connect_dd104g1.php');
+      $sql = "INSERT INTO `stored value record`(`store_no`, `mem_no`, `store_price`, `store_day`) 
+          VALUES (null,{$_SESSION['mem_no']},{$_GET['store_price']},'{$_GET['store_day']}')";
+      $add = $pdo->prepare($sql);
+      $add->execute();
+
+      $sql = "UPDATE `member` SET mem_point = mem_point + {$_GET['store_price']} WHERE mem_no = {$_SESSION['mem_no']}";
+      $points = $pdo->prepare($sql);
+      $points->execute();
+
+      echo "Stored value successfully";
+    } catch (PDOException $th) {
+      echo $th->getMessage(), "<br>";
+    }
+    break;
+  case "meminfos":
+    try {
+      require_once('./connect_dd104g1.php');
+      $sql = "SELECT * FROM `member` WHERE mem_no = {$_SESSION['mem_no']} ";
+      $mem = $pdo->prepare($sql);
+      $mem->execute();
+      $mems = $mem -> fetchAll(PDO::FETCH_ASSOC);
+      echo json_encode($mems);;
+    } catch (PDOException $th) {
+      echo $th->getMessage(), "<br>";
+    }
+    break;
+}
 ?>
