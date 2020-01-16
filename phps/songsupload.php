4e3ba00925a67fdb6749ac69f4b5c59030635ffa
ozzero <eeac8133@gmail.com>
2020-01-16 16:04:18 +0800

    0116add

--- phps/songsupload.php
+++ phps/songsupload.php
@@ -1,22 +1,26 @@
 <?php 
   try {
-	require_once("connectBooks.php");
+  require_once("connectBooks.php");
+  // $song_pic = $_FILES['song_pic']['name'];
+  // $song_addr = $_FILES['song_addr']['name'];
     $sql = "INSERT INTO `total_station_music_library`(`mem_no`,`song_name`,`song_date`,`song_idn`,`song_pic`,`cat_no`,`song_hashtag`,`song_addr`) 
-    VALUES (`mem_no`=:mem_no,'$_GET[song_name]',CURRENT_DATE(),'$_GET[song_idn]','$_GET[song_pic]','$_GET[cat_no]','$_GET[song_hashtag]','./music/$_GET[song_addr]')";
+    VALUES (`mem_no`=:mem_no,'{$_POST["song_name"]}',CURRENT_DATE(),'{$_POST["song_idn"]}','./img/collection/{$_FILES["song_pic"]["name"]}','{$_POST["cat_no"]}','{$_POST["song_hashtag"]}','./music/{$_FILES["song_addr"]["name"]}')";
     $songsadd = $pdo -> prepare($sql);
     // $songsadd->bindValue(":song_name",$_GET["song_name"]);
     // $songsadd->bindValue(":song_no",11);
     // $sgadddate = date("Y-M-D",time());
     // $songsadd->bindValue(":song_date",$sgadddate);
     // $songsadd->bindValue(":song_idn",$_GET["song_idn"]);
-    // $songsadd->bindValue(":song_pic",$_GET["song_pic"]);
+    // $song_pic = ($_FILES['song_pic']['name']);
+    // $songsadd->bindValue('song_pic',$_FILES['song_pic']);
     $songsadd->bindValue(':mem_no',0);
     // $songsadd->bindValue(":donate_acount","0");
     // $songsadd->bindValue(":cat_no",0);
     // $song_hashtag = $_GET["song_hashtag"];
     // $songsadd->bindValue(":song_hashtag",$_GET['song_hashtag']);
     // $songsadd->bindValue(":fav_total","0");
-    // $songsadd->bindValue(":song_addr",$_GET["song_addr"]);
+    // $song_addr = ($_FILES['song_addr']['name']);
+    // $songsadd->bindValue('song_addr',0);
     $songsadd->execute();
     if( $songsadd->rowCount() != 0){
         // $addRow = $songsadd->fetch(PDO::FETCH_ASSOC);
@@ -27,6 +31,11 @@
         // $cat_no=$_GET["cat_no"];
         // $song_hashtag=$_GET["song_hashtag"];
         // $song_addr=$_GET["song_addr"];
+        if($_FILES['song_pic']['error']>0){
+          exit("上傳失敗！");
+        }
+        move_uploaded_file($_FILES['song_pic']['tmp_name'],'../img/collection'.$_FILES['song_pic']['name']);
+        echo '<a href="file/'.$_FILES['song_pic']['name'].'">img/collection'.$_FILES['song_pic']['name'].'</a>';
         echo "Successly";
     }else{
         echo "Failure";
