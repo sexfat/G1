17d305c740ed0a20190927dec6291ff1fcf8b432
ozzero <eeac8133@gmail.com>
2020-01-16 22:07:07 +0800

    0116add

--- phps/songsupload.php
+++ phps/songsupload.php
@@ -12,36 +12,36 @@
     // $songsadd->bindValue(":song_date",$sgadddate);
     // $songsadd->bindValue(":song_idn",$_GET["song_idn"]);
     // $song_pic = ($_FILES['song_pic']['name']);
-    // $songsadd->bindValue('song_pic',$_FILES['song_pic']);
+    // $songsadd->bindValue('song_pic',$_FILES["song_pic"]["name"]);
     $songsadd->bindValue(':mem_no',0);
     // $songsadd->bindValue(":donate_acount","0");
-    // $songsadd->bindValue(":cat_no",0);
+    // $songsadd->bindValue("cat_no",$_POST["cat_no"]);
     // $song_hashtag = $_GET["song_hashtag"];
-    // $songsadd->bindValue(":song_hashtag",$_GET['song_hashtag']);
+    // $songsadd->bindValue("song_hashtag",$_POST["song_hashtag"]);
     // $songsadd->bindValue(":fav_total","0");
     // $song_addr = ($_FILES['song_addr']['name']);
-    // $songsadd->bindValue('song_addr',0);
+    // $songsadd->bindValue('song_addr',$_FILES["song_addr"]["name"]);
     $songsadd->execute();
     if( $songsadd->rowCount() != 0){
         // $addRow = $songsadd->fetch(PDO::FETCH_ASSOC);
         // $addRow = $songsadd->fetchAll(PDO::FETCH_ASSOC);
         // $song_name=$_GET["song_name"];
         // $song_idn=$_GET["song_idn"];
-        // $songpic=$_GET["song_pic"];
-        // $cat_no=$_GET["cat_no"];
-        // $song_hashtag=$_GET["song_hashtag"];
-        // $song_addr=$_GET["song_addr"];
+        // $songpic=$_FILES["song_pic"]["name"];
+        // $cat_no=$_POST["cat_no"];
+        // $song_hashtag=$_POST["song_hashtag"];
+        // $song_addr=$_FILES["song_addr"]["name"];
         if($_FILES['song_pic']['error']>0){
           exit("上傳失敗！");
         }
         move_uploaded_file($_FILES['song_pic']['tmp_name'],'../img/uploadpic/'.$_FILES['song_pic']['name']);
-        echo '<a href="file/'.$_FILES['song_pic']['name'].'">img/uploadpic/'.$_FILES['song_pic']['name'].'</a>';
-
+        // echo '<a href="file/'.$_FILES['song_pic']['name'].'">img/uploadpic/'.$_FILES['song_pic']['name'].'</a>';
+        echo $_FILES['song_pic']['name'].'">img/uploadpic/'.$_FILES['song_pic']['name'];
         if($_FILES['song_addr']['error']>0){
           exit("上傳失敗！");
         }
         move_uploaded_file($_FILES['song_addr']['tmp_name'],'../music/'.$_FILES['song_addr']['name']);
-        echo '<a href="file/'.$_FILES['song_addr']['name'].'">music/'.$_FILES['song_addr']['name'].'</a>';
+        // echo '<a href="file/'.$_FILES['song_addr']['name'].'">music/'.$_FILES['song_addr']['name'].'</a>';
         echo "Successly";
     }else{
         echo "Failure";
