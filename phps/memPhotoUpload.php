12c54b32081a65ee9b3c9423f9bdfd5820a0147e
adzc2568 <adzc2568@gmail.com>
2020-01-18 19:55:26 +0800

    1/18

---
+++ phps/memPhotoUpload.php
@@ -0,0 +1,48 @@
+<?php
+try {
+    require_once("connectBooks.php");
+    session_start();
+    $mem_no = $_SESSION["mem_no"];
+
+    switch ($_FILES['upfile']['error']) {
+        case UPLOAD_ERR_OK:
+            $dir = "../memberRepository/photo/$mem_no";
+            if (file_exists($dir) === false) {
+                mkdir($dir); //make directory
+            } else {
+                $aa = scandir($dir);
+                for ($i=2; $i<=count($aa); $i++){
+                    unlink("$dir/$aa[$i]");
+                }
+            }
+
+            $file_info = pathinfo($_FILES['upfile']['name']);
+            $file_name = "{$mem_no}.{$file_info['extension']}";
+
+            $from = $_FILES['upfile']['tmp_name'];
+            $to = "$dir/" . "$file_name";
+            if (copy($from, $to)) {
+                $sql = "update `member` set `mem_img` = :mem_img where mem_no = $mem_no";
+                $mem_img = $pdo->prepare($sql);
+                $mem_img->bindValue(":mem_img", $file_name);
+                $mem_img->execute();
+                echo "Upload success!";
+            } else {
+                echo "Upload fail!";
+            }
+            break;
+        case UPLOAD_ERR_INI_SIZE:
+            echo "The file is too large, it can't over ". ini_get("upload_max_filesize"). ".";
+            break;
+        case UPLOAD_ERR_FORM_SIZE:
+            echo "The file is too large, it can't over ". $_POST["MAX_FILE_SIZE"]. "."  ;
+            break;
+        case UPLOAD_ERR_PARTIAL:
+            echo "Error during upload, Please upload again.";
+            break;
+        default:
+            echo "['error']: ". $_FILES['upfile']['error'];
+    }
+} catch (PDOException $e) {
+    echo "Upload fail!";
+}
