8780963d18e472ac1c1b960bdb20a3dc9ca83073
Alessandra8566 <money80202@yahoo.com.tw>
2020-01-18 19:28:51 +0800

    1/18

--- phps/connectBooks.php
+++ phps/connectBooks.php
@@ -1,9 +1,7 @@
 <?php
 $dsn = "mysql:host=localhost;port=3306;dbname=dd104g1;charset=utf8";
-
-$user = "onezero";
-$password = "acta228give331";
-
+$user = "sandra";
+$password = "st910018";
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
 ?>
