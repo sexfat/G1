ab727638d38602aac1b0e5038e5c08cba2faa872
yuling13 <spiritusling@gamil.com>
2020-01-19 17:38:18 +0800

    no message

--- phps/connectBooks.php
+++ phps/connectBooks.php
@@ -1,11 +1,7 @@
 <?php
 $dsn = "mysql:host=localhost;port=3306;dbname=dd104g1;charset=utf8";
 $user = "root";
-<<<<<<< HEAD
 $password = "456123789";
-=======
-$password = "liltwind529";
->>>>>>> d3ad60abbc5ab4ccc6936ab746e7ed989dd07725
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
 ?>
