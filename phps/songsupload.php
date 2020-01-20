ed9c049586c4da24e7055044191c97208b3c0338
Ching-yu-25 <ycy0227@gmail.com>
2020-01-21 02:33:55 +0800

    01/21-初步整合完畢 有bug待調

--- phps/songsupload.php
+++ phps/songsupload.php
@@ -1,53 +1,77 @@
 <?php 
+ session_start();
+  function saveImageFile($imageFile){
+      if (!isset($imageFile)) {
+          return null;
+      }
+      if (move_uploaded_file($imageFile['tmp_name'],'../img/uploadpic/'.$imageFile['name'])){
+          return '/img/uploadpic/'.$imageFile['name'];
+      };
+      return null;
+  }
+  function saveMusicFile($file){
+      if (!isset($file)) {
+          return null;
+      }
+      if(move_uploaded_file($file['tmp_name'],'../music/'.$file['name'])){
+          return '/music/'.$file['name'];
+      };
+      return null;
+  }
   try {
-  require_once("connectBooks.php");
-  // $song_pic = $_FILES['song_pic']['name'];
-  // $song_addr = $_FILES['song_addr']['name'];
-    $sql = "INSERT INTO `total_station_music_library`(`mem_no`,`song_name`,`song_date`,`song_idn`,`song_pic`,`cat_no`,`song_hashtag`,`song_addr`) 
-    VALUES (`mem_no`=:mem_no,'{$_POST["song_name"]}',CURRENT_DATE(),'{$_POST["song_idn"]}','./img/collection/{$_FILES["song_pic"]["name"]}','{$_POST["cat_no"]}','{$_POST["song_hashtag"]}','./music/{$_FILES["song_addr"]["name"]}')";
-    $songsadd = $pdo -> prepare($sql);
-    // $songsadd->bindValue(":song_name",$_GET["song_name"]);
-    // $songsadd->bindValue(":song_no",11);
-    // $sgadddate = date("Y-M-D",time());
-    // $songsadd->bindValue(":song_date",$sgadddate);
-    // $songsadd->bindValue(":song_idn",$_GET["song_idn"]);
-    // $song_pic = ($_FILES['song_pic']['name']);
-    // $songsadd->bindValue('song_pic',$_FILES["song_pic"]["name"]);
-    $songsadd->bindValue(':mem_no',0);
-    // $songsadd->bindValue(":donate_acount","0");
-    // $songsadd->bindValue("cat_no",$_POST["cat_no"]);
-    // $song_hashtag = $_GET["song_hashtag"];
-    // $songsadd->bindValue("song_hashtag",$_POST["song_hashtag"]);
-    // $songsadd->bindValue(":fav_total","0");
-    // $song_addr = ($_FILES['song_addr']['name']);
-    // $songsadd->bindValue('song_addr',$_FILES["song_addr"]["name"]);
-    $songsadd->execute();
-    if( $songsadd->rowCount() != 0){
-        // $addRow = $songsadd->fetch(PDO::FETCH_ASSOC);
-        // $addRow = $songsadd->fetchAll(PDO::FETCH_ASSOC);
-        // $song_name=$_GET["song_name"];
-        // $song_idn=$_GET["song_idn"];
-        // $songpic=$_FILES["song_pic"]["name"];
-        // $cat_no=$_POST["cat_no"];
-        // $song_hashtag=$_POST["song_hashtag"];
-        // $song_addr=$_FILES["song_addr"]["name"];
-        if($_FILES['song_pic']['error']>0){
-          exit("上傳失敗！");
-        }
-        move_uploaded_file($_FILES['song_pic']['tmp_name'],'../img/uploadpic/'.$_FILES['song_pic']['name']);
-        // echo '<a href="file/'.$_FILES['song_pic']['name'].'">img/uploadpic/'.$_FILES['song_pic']['name'].'</a>';
-        echo $_POST["song_name"];
-        if($_FILES['song_addr']['error']>0){
-          exit("上傳失敗！");
-        }
-        move_uploaded_file($_FILES['song_addr']['tmp_name'],'../music/'.$_FILES['song_addr']['name']);
-        // echo '<a href="file/'.$_FILES['song_addr']['name'].'">music/'.$_FILES['song_addr']['name'].'</a>';
-        // echo "Successly";
-    }else{
-        echo "Failure";
+    require_once("connectBooks.php");
+    // $addno = $_SESSION["mem_no"];
+     $img = saveImageFile($_FILES['song_pic']);
+     if (!isset($img)) {
+         die("{code: 0, message:'請先上載圖片。'}");
+     }
+     $ret = [];
+    //  $retjson = [];
+     $file = saveMusicFile($_FILES['song_addr']);
+     if (!isset($file)){
+         die("{code: 0, message:'請先上載音樂檔案。'}");
+     }
+    $insertSql = "INSERT INTO `total_station_music_library`(`mem_no`,`song_name`,`song_date`,`song_idn`,`song_pic`,`cat_no`,`song_hashtag`,`song_addr`)".
+                 "VALUES (:memNo, :songName, CURRENT_DATE(), :songIdn, :songPic, :catNo, :songHashTag, :songAddr)";
+    $addSong = $pdo -> prepare($insertSql);
+    $addSong -> bindValue(":memNo", ($ret["mem_no"] = $_SESSION["mem_no"]));
+    $addSong -> bindValue(":songName", ($ret["song_name"] = $_POST["song_name"]));
+    $addSong -> bindValue(":songIdn", ($ret["song_idn"] = $_POST["song_idn"]));
+    $addSong -> bindValue(":songPic", ($ret["imageUrl"] = $img));
+    $addSong -> bindValue(":catNo", ($ret["cat_no"] = $_POST["cat_no"]));
+    $addSong -> bindValue(":songHashTag", ($ret["song_hashtag"] = $_POST["song_hashtag"]));
+    $addSong -> bindValue(":songAddr", ($ret["musicUrl"] = $file));
+    $addSong->execute();
+    if ($addSong -> rowCount() == 0){
+        die("{code: 0, message:'保存失敗。'}");
+        // echo $_POST["song_name"];
     }
+    // 獲取最後插入一條數據的，僅僅本次插入數據
+    $getIdSQL = "select last_insert_id() as id";
+    $dbQuery = $pdo ->query($getIdSQL);
+    $result = $dbQuery -> fetchAll();
+    $ret["id"] = $result[0]["id"];
+
+    $getIdSQL2 = "select last_insert_id() as `cat_no`";
+    $dbQuery2 = $pdo ->query($getIdSQL2);
+    $result = $dbQuery2 -> fetchAll();
+    // $ret["cat_no"] = $result{$_POST["cat_no"]};
+
+    $getIdSQL3 = "select last_insert_id() as `song_hashtag`";
+    $dbQuery3 = $pdo ->query($getIdSQL3);
+    $result = $dbQuery3 -> fetchAll();
+    // $ret["song_hashtag"] = $result[$ret["song_hashtag"]];
+
+    $getIdSQL4 = "select last_insert_id() as `song_idn`";
+    $dbQuery4 = $pdo ->query($getIdSQL4);
+    $result = $dbQuery4 -> fetchAll();
+    $ret["song_idn"] = $result[0]["song_idn"];
+
+    echo json_encode($ret);
+    // echo json_decode($retjson);
   } catch (PDOException $up) {
+
 	echo "例外行號 : ", $up->getLine(),"<br>";
 	echo "例外原因 : ", $up->getMessage(),"<br>";		
   }
-  ?> 
+  ?>
