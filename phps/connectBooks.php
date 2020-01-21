79956cf4fc3975abfe5190d72ae4f9e9edb96b81
ozzero <eeac8133@gmail.com>
2020-01-21 17:38:10 +0800

    aaa

--- phps/connectBooks.php
+++ phps/connectBooks.php
@@ -1,7 +1,7 @@
 <?php
 $dsn = "mysql:host=localhost;port=3306;dbname=dd104g1;charset=utf8";
-$user = "dd104g1";
-$password = "dd104g1";
+$user = "onezero";
+$password = "acta228give331";
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
 ?>
