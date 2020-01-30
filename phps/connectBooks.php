1d5487a27dd3ab0851bc0aa641c896e96bab4344
ozzero <eeac8133@gmail.com>
2020-01-30 15:49:37 +0800

    cb

--- phps/connectBooks.php
+++ phps/connectBooks.php
@@ -1,12 +1,7 @@
 <?php
 $dsn = "mysql:host=localhost;port=3306;dbname=dd104g1;charset=utf8";
-<<<<<<< HEAD
 $user = "onezero";
 $password = "acta228give331";
-=======
-$user = "root";
-$password = "456123789";
->>>>>>> origin/yuling
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
 ?>
