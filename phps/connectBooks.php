3ebc3d21f9c446191508aa5108035ce0eb2134f4
Ching-yu-25 <ycy0227@gmail.com>
2020-01-31 09:38:51 +0800

    01/31-昨天調整的先commit

--- phps/connectBooks.php
+++ phps/connectBooks.php
@@ -1,6 +1,6 @@
 <?php
 $dsn = "mysql:host=localhost;port=3306;dbname=dd104g1;charset=utf8";
-$user = "root  ";
+$user = "root";
 $password = "psw111";
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
